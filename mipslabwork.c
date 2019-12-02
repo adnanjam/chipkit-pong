fee3cd7101f115c4adf2e43f915183040a55711f
Adnan Jamil Ahsan <adnanja@kth.se>
2019-12-02 16:46:57 +0100

    done

--- mipslabwork.c
+++ mipslabwork.c
@@ -19,9 +19,11 @@ int mytime = 0x5957;
 volatile char* portE =  (volatile char *)0xbf886110;
 
 int count = 0;
-int timeoutcount = 0;
-int ballx = 0;
-int bally = 0x00000001;
+int timeoutcunt = 0;
+int trigger = 10;
+
+int ballx = 64;
+int bally = 16;
 int right = 1;
 int down = 1;
 
@@ -41,59 +43,124 @@ void labinit( void ){
   // initialize timer 2:
   T2CON = T2CON | 0b01110000;              // Stop Timer and clear control register,// set prescaler at 1:1, internal clock source
   TMR2 = 0x0;               // Clear timer register 
-  PR2 = 8e6 / 4096;             // Load period register 
+  PR2 = 8e6 / (4096*4);             // Load period register 
   IFSCLR(0) = 0x00000100;  // Clear Timer interrupt status flag 
   T2CONSET = 0x8000;        // Start Timer
 
   return;
 }
-void move_pad(void){
-  int i;
-  for(i = 0; i < height; i++){
-    if(getbtns() == 1){
-      
-      screen[(padAOffset+height)*128 + 128*i] = 0;
-      screen[(padAOffset+height)*128 + 128*i + 1] = 0;
-      padAOffset++;
-      screen[(padAOffset)*128 + 128*i] = 1;
-      screen[(padAOffset)*128 + 128*i + 1] = 1;
 
-
-    }
-    else if(getbtns() == 2){
-      screen[(padAOffset)*128 + 128*i] = 0;
-      screen[(padAOffset)*128 + 128*i + 1] = 0;
-      padAOffset++;
-      screen[(padAOffset+height)*128 + 128*i] = 1;
-      screen[(padAOffset+height)*128 + 128*i + 1] = 1;
-    }
-    else{ 
-      screen[padAOffset*128 + 128*i] = 1;
-      screen[padAOffset*128 + 128*i + 1] = 1;
+void move_pad(int direction, int* offset, int colOffset1, int colOffset2){
+    int heightOffset = direction == 1 ? 0 : height - 1;
+    screen[((*offset)+heightOffset)*128 + colOffset1] = 0;
+    screen[((*offset)+heightOffset)*128 + colOffset2] = 0;
     
-      screen[padBOffset*128 + 128*i - 1] = 1;
-      screen[padBOffset*128 + 128*i - 2] = 1;}
-    }
+    *offset += direction;
+
+    if(*offset < 1) *offset = 1;
+    if(*offset > 31 - height) *offset = 31 - height;
 }
 
-
-void moveX(void){
-  if(right == 1){
-    ballx++;
-    if(ballx == 128) right = 0;
-  }else{
-    ballx--;
-    if(ballx == 0){ right = 1;}
+void move_players(void){
+  if(getsw() == 1){
+    if(getbtns() == 1)move_pad(-1, &padAOffset, 0, 1);
+    if(getbtns() == 2)move_pad(1, &padAOffset, 0, 1);
+  }else {
+    if(getbtns() == 1)move_pad(-1, &padBOffset, -1, -2);
+    if(getbtns() == 2)move_pad(1, &padBOffset, -1, -2);
   }
 }
 
-void moveY(void){
+void show_players(void){
+    int i;
+    for(i = 0; i < height; i++){
+      screen[padAOffset*128 + 128*i] = 1;
+      screen[padAOffset*128 + 128*i + 1] = 1;
+      screen[padBOffset*128 + 128*i - 1] = 1;
+      screen[padBOffset*128 + 128*i - 2] = 1;
+    }
+}
+
+void reset_ball(void){
+  ballx = 64;
+  bally = 16;
+  trigger = 10;
+}
+
+void move_ballX(void){
+  if(right == 1){
+    ballx++;
+    if(ballx >= 127) reset_ball();
+  }
+
+  if(right == 0){
+    ballx--;
+    if(ballx <= 1) reset_ball();
+  }
+}
+
+void move_ballY(void){
   if(down == 1){
-    bally = bally << 1;
-    if(bally == 0x40000000) down = 0;
-  }else{
-    bally =  bally >> 1;
-    if(bally == 0x00000001){ down = 1;}
+    bally++;
+    if(bally > 26  ) down = 0;
+  }
+
+  if(down == 0){
+    bally--;
+    if(bally < 3 )down = 1;
+  }
+}
+
+void show_ball(int value){
+  screen[bally*128 + (ballx)] = value;
+  screen[bally*128 + (ballx)+1] = value;
+  screen[(bally+1)*128 + (ballx)] = value;
+  screen[(bally+1)*128 + (ballx)+1] = value;
+}
+
+void check_collision(){
+  
+  int y = bally + 1;
+  if(right == 1){
+    // Pad B collision
+    if(ballx == 125){
+      if(y >= padBOffset && y < padBOffset + 3){
+        down = 0;
+        right = 0;
+        trigger--;
+      }
+      if(y >= padBOffset + 3 && y < padBOffset + 5) {
+        right = 0;
+        down = -1;
+        trigger--;
+      }
+      if(y >= padBOffset + 5 && y < padBOffset + 8){
+        down = 1;
+        right= 0;
+        trigger--;
+      }
+    }
+  }
+
+  if(right == 0){
+    // Pad A collision
+    if(ballx == 2){
+      if(y >= padAOffset && y < padAOffset + 3){
+        down = 0;
+        right = 1;
+        trigger--;
+      }
+      if(y >= padAOffset + 3 && y < padAOffset + 5) {
+        right = 1;
+        down = -1;
+        trigger--;
+      }
+      if(y >= padAOffset + 5 && y < padAOffset + 8){
+        down = 1;
+        right = 1;
+        trigger--;
+      }
+    }
   }
 }
 
@@ -104,12 +171,25 @@ void labwork( void ){
   if(IFS(0) & 0x100){
     IFSCLR(0) = 0x100;
     count++; 
+    timeoutcunt++;
   }
 
-  if(count == 5){   
-    move_pad();
+  if(timeoutcunt == 3){
+    move_players(); 
+    show_players();
+    timeoutcunt = 0;
+  }
+
+  if(count == trigger){   
+    show_ball(0);
+    move_ballX();
+    check_collision();
+    move_ballY();
+    show_ball(1);
     count = 0;
   }
+
+  if(trigger < 3) trigger = 3;
 
   // display_ball(ballx, bally);
   render();
